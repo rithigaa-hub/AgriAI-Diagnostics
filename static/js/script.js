f837800849fa973617a2a3976f02f3088e0fb78c
rithigaa-hub <rithigaa7@gmail.com>
2026-07-07 12:20:40 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,27 @@
 console.log("AgriAI Diagnostics Loaded");
+
+
+
+const fileInput=document.querySelector(
+    'input[type="file"]'
+);
+
+
+
+if(fileInput)
+{
+
+fileInput.addEventListener(
+"change",
+function(){
+
+console.log(
+"Image Selected:",
+this.files[0].name
+);
+
+
+});
+
+
+}
