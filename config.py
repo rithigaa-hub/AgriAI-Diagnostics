d4756249dd68ac377def62f9bea7f8b2daae241f
rithigaa-hub <rithigaa7@gmail.com>
2026-07-07 12:14:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
+MODEL_PATH = os.path.join(BASE_DIR, "model", "plant_model.keras")
+
+CLASS_NAMES = os.path.join(BASE_DIR, "model", "class_names.json")
+
+DATABASE = os.path.join(BASE_DIR, "database", "plant.db")
+
+IMAGE_SIZE = (224, 224)
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+SECRET_KEY = "agriai_secret_key_2026"
