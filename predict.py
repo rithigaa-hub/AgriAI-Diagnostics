0f907457328a9a3e13f37be3ab4261cced081030
rithigaa-hub <rithigaa7@gmail.com>
2026-07-07 12:17:17 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,60 @@
-print("Prediction")
+import tensorflow as tf
+import numpy as np
+import json
+import cv2
+
+from config import MODEL_PATH, CLASS_NAMES, IMAGE_SIZE
+
+
+# Load model
+
+model = tf.keras.models.load_model(MODEL_PATH)
+
+
+# Load class names
+
+with open(CLASS_NAMES,"r") as file:
+    class_names=json.load(file)
+
+
+
+def predict_image(image_path):
+
+    img=cv2.imread(image_path)
+
+    img=cv2.cvtColor(
+        img,
+        cv2.COLOR_BGR2RGB
+    )
+
+
+    img=cv2.resize(
+        img,
+        IMAGE_SIZE
+    )
+
+
+    img=img/255.0
+
+
+    img=np.expand_dims(
+        img,
+        axis=0
+    )
+
+
+    prediction=model.predict(img)
+
+
+    index=np.argmax(prediction)
+
+
+    confidence=float(
+        np.max(prediction)*100
+    )
+
+
+    disease=class_names[index]
+
+
+    return disease,confidence
